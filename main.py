58b6150f023feee8526f29473be70c4fb1429537
Aurelija Petrauskaite <aurelija35@gmail.com>
2022-12-15 18:42:02 +0200

    Be pakeitimu, dar nebaigta

---
+++ main.py
@@ -0,0 +1,6 @@
+# Pagrindiniame projekto kataloge sukurti failą main.py +
+# Faile main.py importuoti PythonKursas modulį (failą) +
+# Faile main.py inicijuoti Kursas objektą su norimomis savybėmis
+# Faile main.py inicijuoti PythonKursas objektą su norimomis savybėmis
+
+from modules.python_kursas import PythonKursas
